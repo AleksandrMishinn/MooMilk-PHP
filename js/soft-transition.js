2a5cac0e1a31e97ac1355d5fb9189f167118e612
alexandrmishinn <alexandrmishinn@gmail.com>
2019-08-02 09:50:35 +0300

    Smooth scrolling.

--- js/soft-transition.js
+++ js/soft-transition.js
@@ -1,4 +1,6 @@
 $("body").on('click', '[href*="#"]', function(e) {
-    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    $('html,body').stop().animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1000);
     e.preventDefault();
 });
