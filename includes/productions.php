2e0d5def60c433e848e16eaafc355d65811d4688
alexandrmishinn <alexandrmishinn@gmail.com>
2019-08-24 16:10:24 +0300

    Productions slider dev

--- includes/productions.php
+++ includes/productions.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="css/item_slider.css" type="text/css">
 <link rel="stylesheet" href="css/style.css" type="text/css">
+<link rel="stylesheet" href="css/item_slider.css" type="text/css">
 <script src="js/slider-disabled-js.js"></script>
 
 <section>
@@ -9,9 +9,6 @@
         transform="translate(0 -5)" /></svg>
     </section>
 
-    
-
-
 <div class="productions">
 
     <h2 class="title-text center-text">
@@ -19,51 +16,134 @@
     Продукция</h2>
 
 <div id="mi-slider" class="mi-slider">
-    <ul>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
-    <ul>
-       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
-    <ul>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
-    <ul>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
-       <ul>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
-       <ul>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
-    </ul>
 
-    <nav>
+
+<?php 
+
+$connection = include "connection.php"; 
+
+$query_result = mysqli_query($connection, 'SELECT products.name AS product, products.price, products.img, categories.name AS category
+  FROM products AS products
+  LEFT JOIN categories AS categories ON ( products.category = categories.id ) 
+  ORDER BY categories.id');
+
+if ($query_result == false)
+{
+    echo 'Ошибка выполнения запроса.';
+    $connection->close();
+    exit();
+}  
+
+$categories_array = array();
+$products_array = array();
+
+echo "<ul>";
+
+$first_element = true;
+$count_prod_on_category = 0;
+
+while ( ($current_prod = mysqli_fetch_assoc($query_result))) {
+
+    $current_category = $current_prod['category'];
+
+if($first_element === true){
+    $categories_array[] = $current_category;
+    $first_element = false;
+}
+
+    // $products_array[] = array($current_prod['product'], $current_prod['price'], $current_prod['img'], $current_category);
+
+
+if(array_search($current_category, $categories_array) === false){
+    $categories_array[] = $current_category;      
+    $count_prod_on_category = 0;
+    echo "</ul>";
+    echo "<ul>";
+}
+
+if ($count_prod_on_category < 4) {
+    echo "<li><img src='images/".$current_prod['img']."' alt='Продукт' width='50%'><h4>".$current_prod['product']."<br>".$current_prod['price']."</h4></li>";
+}
+
+$count_prod_on_category++;
+};
+
+echo "</ul>";
+
+unset($current_prod);
+unset($current_category);
+
+$connection->close();  
+
+echo "<nav>";
+
+foreach ($categories_array as $current_category) {
+   echo "<a href='#'>".$current_category."</a>"; 
+}
+
+echo "</nav>";
+?>
+
+
+    <!-- <ul>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul>
+    <ul>
+       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+       <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul>
+    <ul>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul>
+    <ul>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul>
+       <ul>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul>
+       <ul>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+        <li><img src="images/sour cream.png" alt="img01" width="60%"><h4>Молоко козье <br> 280 р.</h4></li>
+    </ul> -->
+
+
+
+
+
+
+
+
+   <!--  <nav>
         <a href="#">Популярное</a>
         <a href="#">Молоко</a>
         <a href="#">Кефир</a>
         <a href="#">Йогурт</a>
         <a href="#">Сметана</a>
         <a href="#">Ряженка</a>
-    </nav>
+    </nav> -->
+
+
+
+
+
+
+
 
 </div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
